0597e3ce18a4bf465e13098ec88e32ece7a37908
Devaure <daviddevaure@gmail.com>
2022-01-24 18:26:35 +0100

    create list for my objects and create loop foreach for my call function createCarte

--- js/card.js
+++ js/card.js
@@ -1,39 +1,57 @@
-
-/**
+var myList = [
+    /**
  * Creation de l'objet listDesNoms
  */
-let listDesNoms = {
+{
     name:"Liste des noms",
     description:"Permet d'afficher de façon dynamique quelques noms de personnages",
-    lien:"./html/exercice1.html"
-}
+    lien:"./html/exercice1.html",
+    classNameCard: "cardone",
+    nameButton:"Liste Noms",
+    classNameDescription:"descriptiontwo",
+    classNameButton:"button"
+},
 
 /**
  * Creation de l'objet horloge
  */
-let horloge = {
+{
     name: "Horloge",
     description: "Permet d'afficher une horloge numérique qui affiche l'heure actuelle",
-    lien:"./html/exercice2.html"
-}
+    lien:"./html/exercice2.html",
+    classNameCard: "cardtwo",
+    nameButton:"Horloge",
+    classNameDescription:"description",
+    classNameButton:"buttontwo"
+},
 
 /**
  * Creation de l'objet fizzBuzz
  */
-let fizzBuzz = {
+{
     name: "Fizz Buzz",
     description: "Permet d'afficher Fizz si c'est modulo de 3 sinon si c'est modulo de 5 Buzz sinon si c'est modulo de 15 FizzBuzz",
-    lien:"./html/exercice3.html"
-}
+    lien:"./html/exercice3.html",
+    classNameCard: "cardthree",
+    nameButton:"Fizz Buzz",
+    classNameDescription:"descriptionthree",
+    classNameButton:"buttonthree"
+},
 
 /**
  * Creation de l'objet fizzBuzz
  */
- let fizzBuzzFinal = {
+{
     name: "FizzBuzzFinal",
     description: "Lorsque je clique sur next ou prev, j'affiche Fizz si c'est modulo de 3 sinon si c'est modulo de 5 Buzz sinon si c'est modulo de 15 FizzBuzz",
-    lien:"./html/exercice4.html"
+    lien:"./html/exercice4.html",
+    classNameCard: "cardfor",
+    nameButton:"Fizz Buzz Final",
+    classNameDescription:"descriptionfor",
+    classNameButton:"buttonfor"
 }
+]
+
 
 /**
  * Fonction permettant de créer les cartes
@@ -54,7 +72,8 @@ let fizzBuzz = {
     card.innerHTML += "<a class="+classNameButton+" href="+lien+">"+nameButton+"</a>";
     container.appendChild(card);
 }
-createCard(horloge.name,horloge.description,horloge.lien,"cardtwo", "Horloge","descriptiontwo", "buttontwo");
-createCard(listDesNoms.name,listDesNoms.description,listDesNoms.lien,"cardone","Liste Noms","description", "button");
-createCard(fizzBuzz.name,fizzBuzz.description,fizzBuzz.lien,"cardthree", "Fizz Buzz","descriptionthree", "buttonthree");
-createCard(fizzBuzzFinal.name,fizzBuzzFinal.description,fizzBuzzFinal.lien,"cardfor", "Fizz Buzz Final", "descriptionfor", "buttonfor");
+
+myList.forEach(element => {
+    createCard(element.name,element.description,element.lien,element.classNameCard,element.nameButton,element.classNameDescription, element.classNameButton);
+
+});
